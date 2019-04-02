e2e7b388168bd7791b47fdc07bbf7286cf933df6
mloan2017 <mloan2017@gmail.com>
2019-04-02 10:30:07 +0700

    unit test added
    unit test added

---
+++ Devop/Test/Test.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+namespace Test
+{
+   [TestFixture()]
+   public class Test
+   {
+      [Test()]
+      public void TestCase()
+      {
+			
+      }
+
+
+		[Test()]
+        public void TestAdd()
+        {
+			Assert.AreEqual(28, 28); 
+        }
+
+
+
+		[Test()]
+        public void TestSub()
+        {
+			Assert.AreEqual(28, 20); 
+        }
+   }
+}
